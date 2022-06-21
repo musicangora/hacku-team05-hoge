d2621c7f0485aa0a0852550a2e3ea283b45ad03e
r5Rin <nemonora-weed37@outlook.jp>
2022-06-22 03:38:20 +0900

    お題表示・参加者数・回答ボックスコンポーネント作成

---
+++ components/UserList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button @click="toggleList">現在のメンバー: {{ members.length }}人(クリックして一覧表示)</button>
+    <ul v-if="expanded">
+      <li v-for="(member, key) in members" :key="key">{{ member.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'UserList',
+  data () {
+    return {
+      members: [{ id: '123abc', name: 'foo' }, { id: 'bcd456', name: 'bar' }],
+      expanded: false
+    }
+  },
+  created () {
+    // メンバーを取得してmembersに入れる処理
+  },
+  methods: {
+    toggleList () {
+      this.expanded = !this.expanded
+    }
+  }
+}
+
+</script>
--- pages/_id/thinkAnser/index.vue
+++ pages/_id/thinkAnser/index.vue
@@ -1,6 +1,8 @@
 <template>
   <div>
     <p>ここは,自分の回答を入力するページ</p>
+    <NowQuestion />
+    <TextInput :type="'answer'" @postComplete="showSnackbar" />
     <!-- 以下実際はタイマーでページ遷移 -->
     <NuxtLink to="/:id/decideAnser">時間が来ました</NuxtLink>
   </div>
