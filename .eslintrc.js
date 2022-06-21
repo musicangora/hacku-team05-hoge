f469b4cd4e839069020dace5c20ccb259af906b1
r5Rin <nemonora-weed37@outlook.jp>
2022-06-22 00:38:48 +0900

    eslintrcを追記

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
   // add your custom rules here
   rules: {
     'vue/singleline-html-element-content-newline': 'off',
-    'vue/multiline-html-element-content-newline': 'off'
+    'vue/multiline-html-element-content-newline': 'off',
+    'vue/multi-word-component-names': 'off'
   }
 }
