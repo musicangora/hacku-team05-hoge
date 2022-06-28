dadf661328fc0636c97409f7af038f30e0a59324
Miiya <40447362+musicangora@users.noreply.github.com>
2022-06-28 22:33:58 +0900

    Add UI styles for top page

---
+++ tailwind.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  mode: 'jit',
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        'my-black': '#080544',
+        'my-yellow': '#FFDC76'
+      }
+    }
+  },
+  variants: {
+    backgroundColor: ['responsive', 'hover', 'focus', 'active'],
+    textColor: ['responsive', 'hover', 'focus', 'active'],
+    boxShadow: ['responsive', 'hover', 'focus', 'active'],
+
+    extend: {}
+  },
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        '.text-ol-white-2': {
+          textShadow:
+            '2px 2px 0px #FEFEFE, -2px 2px 0px #FEFEFE, 2px -2px 0px #FEFEFE, -2px -2px 0px #FEFEFE'
+        },
+        '.text-ol-white-4': {
+          textShadow:
+            '4px 4px 0px #FEFEFE, -4px 4px 0px #FEFEFE, 4px -4px 0px #FEFEFE, -4px -4px 0px #FEFEFE'
+        },
+        '.button-shadow': {
+          boxShadow: '0px 4px 0px #080544'
+        },
+        '.button-shadow-none': {
+          boxShadow: '0px 0px 0px #080544'
+        }
+      }
+
+      addUtilities(newUtilities)
+    }
+  ]
+}
