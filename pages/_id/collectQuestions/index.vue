916aebbec806ea266ea436bb949db4162a85dde2
r5Rin <nemonora-weed37@outlook.jp>
2022-06-30 22:27:57 +0900

    Update index.vue

--- pages/_id/collectQuestions/index.vue
+++ pages/_id/collectQuestions/index.vue
@@ -56,7 +56,7 @@
 </template>
 
 <script>
-import ListPanel from '../../../components/listPanel.vue'
+import ListPanel from '../../../components/istPanel.vue'
 export default {
   name: 'IndexPage',
   components: { ListPanel },
